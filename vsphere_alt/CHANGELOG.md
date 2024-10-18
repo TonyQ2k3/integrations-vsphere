d46c1f8e560783e70acc929e4eee2bb6502191a1
TonyQ2k3 <ntbquoc2015@gmail.com>
2024-10-18 14:56:56 +0700

    Added vsphere_alt module

---
+++ vsphere_alt/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG - vSphere_Alt
+
+## 1.0.0 / YYYY-MM-DD
+
+***Added***:
+
+* Initial Release
+
---
+++ vsphere_alt/datadog_checks/vsphere_alt/__init__.py
@@ -0,0 +1,5 @@
+
+from .__about__ import __version__
+from .check import VsphereAltCheck
+
+__all__ = ['__version__', 'VsphereAltCheck']
